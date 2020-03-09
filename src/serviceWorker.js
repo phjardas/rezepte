ea079b720cffcfaf8293299b0ce95f549904af75
Philipp Jardas <philipp@jardas.de>
2020-03-09 17:23:22 +0100

    sw: more logging

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,14 +21,15 @@ export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    console.log('[sw] public url', publicUrl.origin, 'vs location', window.location.origin, 'same', publicUrl.origin === window.location.origin);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      console.log('[sw] skipping service worker registration because origin does not match');
       return;
     }
 
+    console.log('[sw] adding event listener');
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       console.log('[sw] loading service worker from', swUrl);
